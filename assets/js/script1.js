6cbe09423f03a617255b2a6919ba97be98e89148
orbit-platform <maibam.sharat@gmail.com>
2024-11-26 16:18:43 +0530

    about page:scrolls up page when "Read Less" button is clicked

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -8,10 +8,13 @@ function changeReadMore() {
     if (rmcontent.style.display === 'none'
         || rmcontent.style.display === '') {
             rmcontent.style.display = 'block';
-        span1.style.display = "none";
+        span1.style.display = 'none';
         rmbutton.textContent = 'Read Less';
+        
     } else {
         rmcontent.style.display = 'none';
-        rmbutton.textContent = 'Read More';        
+        span1.style.display = 'inline';
+        rmbutton.textContent = 'Read More'; 
+        window.scrollTo(900,900);       
     }
 }
